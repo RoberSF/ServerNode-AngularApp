f14ceff719f003df2eab488d7a5b4c2051a4b59e
roberto.sanchez <rober.sanchez.figueroa@gmail.com>
2020-05-01 18:23:04 +0200

    conseguir token de google en mi backend

--- config/config.js
+++ config/config.js
@@ -1,3 +1,8 @@
 
 
 module.exports.SEED = '@este-es-mi-seed'
+
+//Google
+
+module.exports.CLIENT_ID = '912602372070-uu6j4sri55bl9b279g9hk9fvi7mh3clm.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = '912602372070-uu6j4sri55bl9b279g9hk9fvi7mh3clm.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -4,10 +4,73 @@ var app = express();
 var bcrypt = require('bcryptjs'); // Librería para encriptar la contraseña
 var Usuario = require('../models/usuario'); // me permite usar todo lo que hay en usuarios.js
 var jwt = require('jsonwebtoken') // librería para crear el token 
-var SEED = require('../config/config').SEED;
+var SEED = require('../config/config').SEED; // es una constante por eso la tengo en un archivo aparte
+
+
+//Google
+var CLIENT_ID = require('../config/config').CLIENT_ID; // es una constante por eso la tengo en un archivo aparte
+var GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET; // es una constante por eso la tengo en un archivo aparte
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+
+//***********************************************************************************************************
+//                                      Autenticacio Google */
+//********************************************************************************************************** */
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+
+    const payload = ticket.getPayload(); // aquí vamos a tener la info del usuario
+    // const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true,
+
+    }
+  }
+
+
+  app.post('/google', (req, res,next) =>{
+    var token = req.body.token;
+    const oAuth2Client = new OAuth2Client(
+       CLIENT_ID,
+       GOOGLE_SECRET
+     );
+     const tiket = oAuth2Client.verifyIdToken({
+       idToken: token
+       //audience: GOOGLE_CLIENT_ID
+     });
+     tiket.then(data =>{
+       res.status(200).json({
+         ok: true,
+         tiket: data.payload
+       })
+     }).catch(err => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Token no válido',
+                errors: err
+            });
+        }
+    });
+  });
 
 
 
+//***********************************************************************************************************
+//                                      Autenticacio Normal */
+//********************************************************************************************************** */
 app.post('/',(request,response) => {
 
     var body = request.body; // esto sólo va a funcionar si tengo el body-parser
