4617498ea1ed6d27c347ee9ace13848ca1072be3
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2020-10-14 01:00:43 +0200

    categoria added to blog

--- models/blogPost.js
+++ models/blogPost.js
@@ -6,14 +6,15 @@ var Schema = mongoose.Schema; // Para definir esquemas primero hay que importar
 
 
 var postSchema = new Schema({
-    title:{type: String, unique:true, required: [true, 'Necesitas un título']}, 
-    intro:{type: String, unique:true, required: [true, 'La intro es necesaria']},
-    contenido:{type: String, required: [true, 'El contenido es necesaria']},
-    categoria:{type: String, required: [true, 'La categoria es necesaria']},  
+    title: { type: String, unique: true, required: [true, 'Necesitas un título'] },
+    intro: { type: String, unique: true, required: [true, 'La intro es necesaria'] },
+    contenido: { type: String, required: [true, 'El contenido es necesaria'] },
+    // categoria:{type: String, required: [true, 'La categoria es necesaria']},
+    categoria: { type: Schema.Types.ObjectId, ref: 'Categoria', required: true }, //es tipo object por que devuelve una info de un objeto entero, con populate trabajamos en la ruta
     date: { type: Date, required: [true, 'La fecha es necesaria'] },
-    comentarios:{type: String, requiered:false},  
-    img:{type: String, requiered:false}, 
-}); 
+    comentarios: { type: String, requiered: false },
+    img: { type: String, requiered: false },
+});
 
 
 module.exports = mongoose.model('Post', postSchema)
