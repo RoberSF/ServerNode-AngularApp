7462f4209318f0ed930bd4adc8147024b5be21ba
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2020-10-13 13:38:12 +0200

    let to var

--- middlewares/autentication.js
+++ middlewares/autentication.js
@@ -1,6 +1,7 @@
 var jwt = require('jsonwebtoken') // librería para crear el token 
 var SEED = require('../config/config').SEED;
 
+// Middleware  => validaciones personalizadas para nuestras rutas y lo que necesite
 
 
 //Esta es cuando mandas el token por la url
@@ -36,23 +37,23 @@ var SEED = require('../config/config').SEED;
 
 exports.verificaToken = function(req, res, next) {
     // var token = req.query.token;
-    var token = req.headers.authorization;
+    let token = req.headers.authorization;
     jwt.verify(token, SEED, (err, decoded) => {
         if (err) {
             return res.status(401).json({
-            ok: false,
-            mensaje: 'Token incorrecto',
-            errors: err
-        });
-    }
-    req.usuario = decoded.usuario; // no das la información que hemos enviado 
-    next();
+                ok: false,
+                mensaje: 'Token incorrecto',
+                errors: err
+            });
+        }
+        req.usuario = decoded.usuario; // no das la información que hemos enviado 
+        next();
     });
-   }
+}
 
 
- // ==========================================
- // VERIFICA ADMIN_ROLE
+// ==========================================
+// VERIFICA ADMIN_ROLE
 // Enviar token por headers
 // Es otro middelware para verificar que cumple una condicion y dejar pasar al resto del código
 // ==========================================
@@ -63,22 +64,22 @@ exports.verificaADMIN_ROLE = function(req, res, next) {
 
     var usuario = req.usuario;
 
-    if ( usuario.role === 'ADMIN_ROLE') {
+    if (usuario.role === 'ADMIN_ROLE') {
         next();
         return;
     } else {
         return res.status(401).json({
             ok: false,
             mensaje: 'User con permisos insuficientes',
-            
-    });
 
-   }
+        });
+
+    }
 
 }
 
 
- // ==========================================
+// ==========================================
 // VERIFICA ADMIN_ROLE o mismo usuario
 // Enviar token por headers
 // Es otro middelware para verificar que cumple una condicion y dejar pasar al resto del código
@@ -92,16 +93,16 @@ exports.verificaADMIN_ROLE_o_MISMOuSUARIO = function(req, res, next) {
 
     var id = req.params.id
 
-    if ( usuario.role === 'ADMIN_ROLE' || usuario._id === id) {
+    if (usuario.role === 'ADMIN_ROLE' || usuario._id === id) {
         next();
         return;
     } else {
         return res.status(401).json({
             ok: false,
             mensaje: 'User con permisos insuficientes',
-            
-    });
 
-   }
+        });
+
+    }
 
 }
