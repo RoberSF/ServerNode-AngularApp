49368016e3aa0be534036564b1c36feb7cc499de
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2020-10-14 01:02:43 +0200

    little changes

--- routes/hospital.js
+++ routes/hospital.js
@@ -21,31 +21,32 @@ app.get('/', (request, response) => {
     since = Number(since);
 
     Hospital.find({}) //el .find es por mongoo. Las caracteristicas es para que se muestre sólo eso. Yo no quiero que me enseñe su password por ejemplo
-            .skip(since) // con esto le estoy diciendo que se salte los x registros "localhost:4000/usuario?since=5"
-            .limit(5) // le estoy diciendo que me muestre sólo los 5 primeros registros. Siguiente paso? var = since
-            .populate('usuario', 'nombre email') // que tabla y que campos quiero de la tabla relacionada. Así cuando haga la petición get voy a saber quien la hizo
-            .exec(
-            
-            (error,hospitales) => { 
-    
-            if ( error ) {
-                return response.status(500).json({
-                    ok: false,
-                    mensaje: 'Error cargando hospitales',
-                    errors: error
-                });
-            }
+        //  .sort() => para ordenar la devolución de los resultados. Ver ejemplo
+        .skip(since) // con esto le estoy diciendo que se salte los x registros "localhost:4000/usuario?since=5"
+        .limit(5) // le estoy diciendo que me muestre sólo los 5 primeros registros. Siguiente paso? var = since
+        .populate('usuario', 'nombre email') // que tabla y que campos quiero de la tabla relacionada. Así cuando haga la petición get voy a saber quien la hizo
+        .exec(
 
-            Hospital.count({}, (error, count) => {
+            (error, hospitales) => {
 
-                response.status(200).json({
-                    ok: true,
-                    hospitales: hospitales,
-                    total: count,// numero de usuario totales
-                });
+                if (error) {
+                    return response.status(500).json({
+                        ok: false,
+                        mensaje: 'Error cargando hospitales',
+                        errors: error
+                    });
+                }
+
+                Hospital.count({}, (error, count) => {
+
+                    response.status(200).json({
+                        ok: true,
+                        hospitales: hospitales,
+                        total: count, // numero de usuario totales
+                    });
+                })
             })
-        }) 
-    })
+})
 
 
 //*****************************************************************************************************
@@ -56,16 +57,16 @@ app.get('/:id', (req, resp) => {
     var id = req.params.id;
     Hospital.findById(id)
         .populate('usuario', 'nombre img email')
-        .exec( (error, hospital) => {
+        .exec((error, hospital) => {
             if (error) {
                 return resp.status(500).json({
-                    ok:false,
+                    ok: false,
                     menssage: 'Error al buscar Hospital',
                     errors: error
                 });
             }
 
-            if ( !hospital ) {
+            if (!hospital) {
                 return resp.status(400).json({
                     ok: false,
                     menssage: 'El hospital con id' + id + 'no existe'
@@ -73,7 +74,7 @@ app.get('/:id', (req, resp) => {
             }
 
             resp.status(200).json({
-                ok:true,
+                ok: true,
                 hospital: hospital
             });
         })
@@ -87,13 +88,13 @@ app.get('/:id', (req, resp) => {
 app.post('/', middlewareAutentication.verificaToken, (request, response) => {
 
     var body = request.body;
-    var hospital = new Hospital({  // con esto creamos esta referencia para despues guardarlo
+    var hospital = new Hospital({ // con esto creamos esta referencia para despues guardarlo
         nombre: body.nombre,
         usuario: request.usuario._id
     });
 
-    hospital.save((error,hospitalGuardado) => {
-        if ( error ) {
+    hospital.save((error, hospitalGuardado) => {
+        if (error) {
             return response.status(400).json({
                 ok: false,
                 mensaje: 'Error al crear hospital',
@@ -115,10 +116,10 @@ app.post('/', middlewareAutentication.verificaToken, (request, response) => {
 app.put('/:id', middlewareAutentication.verificaToken, (request, response) => {
 
     var id = request.params.id; // para recibirlo de la URL 
-    var body = request.body; 
+    var body = request.body;
 
     Hospital.findById(id, (error, hospital) => { // está función es algo de mongoose. Usuario hace referencia al model. El usuario de dentro del callback es el usuario que encuentra por el id
-        if ( error ) {
+        if (error) {
             return response.status(500).json({
                 ok: false,
                 mensaje: 'Error al buscar hospital',
@@ -126,29 +127,29 @@ app.put('/:id', middlewareAutentication.verificaToken, (request, response) => {
             });
         }
 
-        if ( !hospital ) { // sí, el usuario viene null 
-            
+        if (!hospital) { // sí, el usuario viene null 
+
             return response.status(400).json({
                 ok: false,
                 mensaje: 'El hospital con el' + id + 'no existe',
-                errors: {menssage: 'No existe con este Id'}
+                errors: { menssage: 'No existe con este Id' }
             });
         }
         hospital.nombre = body.nombre;
         // hospital.img = body.img;
         hospital.usuario = request.usuario._id; // de la request quiero esto
 
-        hospital.save((error,hospitalGuardado) => {
-            if ( error ) {
+        hospital.save((error, hospitalGuardado) => {
+            if (error) {
                 return response.status(400).json({
                     ok: false,
                     mensaje: 'Error al actualizar hospital',
                     errors: error
                 });
             }
-        response.status(200).json({
-            ok: true,
-            hospital: hospitalGuardado,
+            response.status(200).json({
+                ok: true,
+                hospital: hospitalGuardado,
             });
         });
     });
@@ -158,26 +159,26 @@ app.put('/:id', middlewareAutentication.verificaToken, (request, response) => {
 //                                          Borrar Hospitales
 //*****************************************************************************************************
 
-app.delete('/:id', middlewareAutentication.verificaToken, (request,response) => {
+app.delete('/:id', middlewareAutentication.verificaToken, (request, response) => {
 
     var id = request.params.id; // el id tiene que ser el mismo nombre que aparece en la url :id
 
     Hospital.findByIdAndRemove(id, (error, hospitalBorrado) => {
-            if ( error ) {
+        if (error) {
             return response.status(500).json({
                 ok: false,
                 mensaje: 'Error al borrar hospital',
                 errors: error
             });
-            }
+        }
 
-        if ( !hospitalBorrado) {
+        if (!hospitalBorrado) {
             return response.status(400).json({
-                ok:false,
+                ok: false,
                 mensaje: 'No existe un hospital con ese id',
-                errors: {message: 'No existe un hospital con ese id'}
+                errors: { message: 'No existe un hospital con ese id' }
             });
-        } 
+        }
 
         response.status(201).json({
             ok: true,
